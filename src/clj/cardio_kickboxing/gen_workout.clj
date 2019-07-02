8f49b41fb46f0a9db6a531cac27fd4fe84ab0cc1
Patrick Trantham <patricktrantham@gmail.com>
2019-07-01 21:07:17 -0500

    Improve workout generation in many ways
    Instead of randomly selecting exercises, use exercise difficulty and
    lazy sequences to generate a more consistent workout with less
    repitition.  A sequence of difficulties is used to form the outline for
    the workout so that difficulty can ramp up and down.

--- src/clj/cardio_kickboxing/gen_workout.clj
+++ src/clj/cardio_kickboxing/gen_workout.clj
@@ -4,14 +4,46 @@
 
 (def sample-workout
   {:title "Sample Workout #1"
-   :rest-seconds 10
-   :active-seconds 20
+   :rest-seconds 30
+   :active-seconds 120
    :exercises nil})
 
-(defn select-exercises []
-  (let [exercise-list (db/get-exercises)]
-    (take 15 (repeatedly #(rand-nth exercise-list)))))
+;; TODO: Make this more dynamic
+(defn generate-sequence [length]
+  "TODO: Generate a sequence of difficulties to outline a workout"
+  (interleave '(1 1 1 1 1) '(2 2 2 2 3) '(1 1 3 1 1)))
+
+(defn get-exercises-of-difficulty
+  "Takes collection of exercises and a difficulty, returns a shuffled lazy seq of exercises matching the difficulty"
+  [exercises difficulty]
+  (cycle (shuffle (filter #(= difficulty (:difficulty %)) exercises))))
+
+(defn build-lazy-seqs
+  "Takes collection of exercises, returns map of difficulty to lazy seq of exercises"
+  [exercises]
+  (let [difficulties [1 2 3]]
+    (into {}
+          (map (fn [diff] [diff (get-exercises-of-difficulty exercises diff)])
+               difficulties))))
+
+(defn exercises-by-sequence
+  "Takes collection of exercises and a sequence of difficulties, returns a list of exercises matching the difficulties"
+  [exercises diff-seq]
+  (loop [workout []
+         remaining diff-seq
+         lazy-seqs (build-lazy-seqs exercises)]
+    (if (empty? remaining)
+      workout
+      (do
+        (let [diff (first remaining)
+              exer (first (get lazy-seqs diff))
+              next-seqs (update lazy-seqs diff rest)]
+          (recur
+            (conj workout exer)
+            (rest remaining)
+            next-seqs))))))
 
 (defn gen-workout []
-  (let [workout sample-workout]
-    (assoc workout :exercises (select-exercises))))
+  (let [workout sample-workout
+        length 15]
+    (assoc workout :exercises (exercises-by-sequence (db/get-exercises) (generate-sequence length)))))
